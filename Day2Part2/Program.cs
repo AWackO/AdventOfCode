3e0f711b80ecb3c28528c9a4818e2876b00c9424
Aleksa <aleksa.milenkovic@atessoft.rs>
2024-02-29 12:22:48 +0100

    Day 2 Part 2 finished

---
+++ Day2Part2/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+string filePath = "D:\\acko\\Programiranje\\dotnet_projekti\\AdventOfCode\\Day2Part2\\input.txt";
+
+string[] lines = File.ReadAllLines(filePath);
+
+int sum = 0;
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(':', StringSplitOptions.TrimEntries);
+    string[] sets = parts[1].Split(';', StringSplitOptions.TrimEntries);
+
+    int maxRed = 0;
+    int maxGreen = 0;
+    int maxBlue = 0;
+
+    foreach (string set in sets)
+    {
+        string[] cubes = set.Split(',', StringSplitOptions.TrimEntries);
+
+        foreach (string cube in cubes)
+        {
+            string[] cubeInfo = cube.Split(' ', StringSplitOptions.TrimEntries);
+            int currentColorCount = int.Parse(cubeInfo[0]);
+            string color = cubeInfo[1];
+
+            switch (color)
+            {
+                case "red":
+                    maxRed = Math.Max(currentColorCount, maxRed);
+                    break;
+                case "green":
+                    maxGreen = Math.Max(currentColorCount, maxGreen);
+                    break;
+                case "blue":
+                    maxBlue = Math.Max(currentColorCount, maxBlue);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    sum += (maxRed * maxGreen * maxBlue);
+}
+
+Console.WriteLine(sum);
